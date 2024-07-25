5fab56f1d216e98ee6c5ce560f208a408e9589dc
小松 <1829134124@qq.com>
2024-07-26 00:42:45 +0800

    second commit

--- lib/flutter_simple_websocket.dart
+++ lib/flutter_simple_websocket.dart
@@ -1,3 +1,13 @@
 library flutter_simple_websocket;
 
-// 要暴露的类
+// 要暴露的client类
+export './microService/service/client/websocket/WebsocketClientManager.dart';
+export './microService/service/client/websocket/ChatWebsocketClient.dart';
+export './microService/service/client/websocket/WebsocketClient.dart';
+
+// 要暴露的server类
+export './microService/service/server/websocket/WebsocketServer.dart';
+export './microService/service/server/websocket/ChatWebsocketServer.dart';
+export './microService/service/server/websocket/WebsocketServer.dart';
+
+// 要暴露的其他类
